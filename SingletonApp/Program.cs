ea675d06d2370cdcb270d79302107d6b8a49d5e8
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-06-25 16:48:16 +0900

    Add project files.

---
+++ SingletonApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SingletonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton employee = Singleton.GetInstance;
+            employee.PrintDetails("from employee");
+
+
+            Singleton student = Singleton.GetInstance;
+            student.PrintDetails("from student");
+            Singleton employee1 = Singleton.GetInstance;
+            employee1.PrintDetails("from employee");
+
+
+            Singleton student1 = Singleton.GetInstance;
+            student1.PrintDetails("from student");
+        }
+    }
+}
---
+++ SingletonApp/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SingletonApp
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+
+        #region not therad safe
+
+        //private static Singleton instance = null;
+
+        //public static Singleton GetInstance
+        //{
+        //    get
+        //    {
+        //        if (instance == null)
+        //            instance = new Singleton();
+        //        return instance;
+        //    }
+        //}
+        #endregion
+
+
+        #region fully lazy instantiation
+
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton GetInstance { get { return lazy.Value; } }
+        #endregion
+
+
+
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
